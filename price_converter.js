d28e43c0fde8541f410a788f25f34a2941d17dd3
Jamie <ScruffyTheMoose@users.noreply.github.com>
2021-12-11 18:36:15 -0600

    add getPrice

--- price_converter.js
+++ price_converter.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 // Exchange Rates: http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1
-// Countries: https://restcountries.eu/rest/v2/currency/${currencyCode}
+// Stock Price: https://financialmodelingprep.com/api/v3/quote-short/${symbol}?apikey=${APIkey}
+// API key: f02d97317321d926e73f2343ac8a5b65
 
 const getExchangeRate = async (fromCurrency, toCurrency) => {
     const response = await axios.get('http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1');
@@ -13,11 +14,12 @@ const getExchangeRate = async (fromCurrency, toCurrency) => {
     return exRate;
 }
 
-// getExchangeRate('USD', 'EUR');
+// console.log(getExchangeRate('USD', 'EUR'));
 
-const getCountries = async (toCurrency) => {
-    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${toCurrency}`);
-    console.log(response.data);
+const getPrice = async (stockSymbol, APIkey) => {
+    const response = await axios.get(`https://financialmodelingprep.com/api/v3/quote-short/${stockSymbol}?apikey=${APIkey}`);
+    const price = response.data[0]['price'];
+    return price;
 }
 
-getCountries('USD');
+// console.log(getPrice('AAPL', 'f02d97317321d926e73f2343ac8a5b65'));
