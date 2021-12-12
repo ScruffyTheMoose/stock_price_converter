9135e8856f5584b858e68d144e3934fcaa7c9518
Jamie <ScruffyTheMoose@users.noreply.github.com>
2021-12-11 18:05:37 -0600

    add async to getExchangeRate

--- currency_converter.js
+++ currency_converter.js
@@ -3,14 +3,14 @@ const axios = require('axios');
 // Exchange Rates: http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1
 // Countries: https://restcountries.eu/rest/v2/currency/${currencyCode}
 
-const getExchangeRate = (fromCurrency, toCurrency) => {
-    axios.get('http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1').then((response) => {
-        const rate = response.data.rates;
-        const euro = 1 / rate[fromCurrency];
-        const exRate = euro * rate[toCurrency];
+const getExchangeRate = async (fromCurrency, toCurrency) => {
+    const response = await axios.get('http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1');
 
-        console.log(exRate);
-    });
+    const rate = response.data.rates;
+    const euro = 1 / rate[fromCurrency];
+    const exRate = euro * rate[toCurrency];
+
+    return exRate;
 }
 
 getExchangeRate('USD', 'EUR');
